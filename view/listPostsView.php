5f2e413d5d472822b9545bc24515f872d348e793
emi-sabathier <sabathier.emilie@gmail.com>
2019-03-10 18:39:34 +0100

    Création de la view listPostsView

---
+++ view/listPostsView.php
@@ -0,0 +1,28 @@
+<?php $title = 'Liste des derniers billets';?>
+
+<?php ob_start();?>
+<h1>Bienvenue !</h1>
+
+<div class="content">
+    <h3>Liste des billets</h3>
+
+    <?php
+foreach ($posts as $post) {?>
+
+    <p>
+        <strong><?=htmlspecialchars($post['title'])?></strong>
+        écrit par
+        <?=htmlspecialchars($post['author'])?>
+        le
+        <?=htmlspecialchars($post['date_fr'])?>
+    </p>
+    <p><a href="index.php?action=post&idPost=<?=$post['id']?>">Voir la recette</a></p>
+    <?php
+}
+?>
+</div>
+
+
+<?php $content = ob_get_clean();?>
+
+<?php require 'template.php';?>
