5cddb01f611a5301aab66ca2cdd70a3d1665f98a
emi-sabathier <sabathier.emilie@gmail.com>
2019-03-10 18:18:41 +0100

    Ajout méthode listPosts() à PostsController

--- controller/PostsController.php
+++ controller/PostsController.php
@@ -1,10 +1,20 @@
 <?php
 namespace Blog\controller;
+
 use Blog\model\PostsManager;
 
 require_once 'model/PostsManager.php';
 
-class PostsController {
+class PostsController
+{
+    public function listPosts()
+    {
 
+        $postsManager = new PostsManager();
+        $posts = $postsManager->getListPosts();
+
+        require 'view/listPostsView.php';
+
+    }
 
 }
