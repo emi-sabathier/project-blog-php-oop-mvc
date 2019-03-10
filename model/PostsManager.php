05af4b6335f21daf2a8bbdaa3269ee56a51dbe82
emi-sabathier <sabathier.emilie@gmail.com>
2019-03-10 18:34:12 +0100

    Ajout méthode getListPosts contenant sa requête

--- model/PostsManager.php
+++ model/PostsManager.php
@@ -7,7 +7,16 @@ require_once 'Manager.php';
 class PostsManager extends Manager 
 {
 
-    
+    public function getListPosts() {
+
+        $db = $this->dbconnect(); // this.dbConnect()
+        $q = $db->query('SELECT id, title, author, DATE_FORMAT(creation_date, \'%d/%m/%Y %H:%i:%s\') AS date_fr FROM posts ORDER BY creation_date DESC');
+        $posts = $q->fetchAll();
+        
+        return $posts;
+    }
+
+   
 }
 
 
