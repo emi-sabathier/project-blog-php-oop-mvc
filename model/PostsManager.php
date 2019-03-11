bd089a62ff89a8f8deafe34527e7c7c29d9955be
emi-sabathier <sabathier.emilie@gmail.com>
2019-03-11 10:44:42 +0100

    Mise à jour requête méthode getPost

--- model/PostsManager.php
+++ model/PostsManager.php
@@ -19,7 +19,7 @@ class PostsManager extends Manager
     public function getPost($idPost) {
 
         $db = $this->dbConnect();
-        $q = $db->prepare('SELECT id, title, author, content, DATE_FORMAT(post_date, \'%d/%m/%Y %H:%i:%s\') AS post_date_fr FROM posts WHERE id = ?');
+        $q = $db->prepare('SELECT posts.id, posts.title, posts.author_id, users.username, posts.content, DATE_FORMAT(post_date, \'%d/%m/%Y %H:%i:%s\') AS post_date_fr FROM posts INNER JOIN users ON posts.author_id = users.id WHERE posts.id = ?');
         $q->execute(array($idPost));
         $post = $q->fetch(); 
 
