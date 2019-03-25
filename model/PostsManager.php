0ddd7366ba1472500d0cdee46d8325b0eefe9773
emi-sabathier <sabathier.emilie@gmail.com>
2019-03-25 15:37:51 +0100

    Simplification requêtes post

--- model/PostsManager.php
+++ model/PostsManager.php
@@ -8,48 +8,26 @@ class PostsManager extends Manager
 {
 
     public function getListPosts() {
-
+        // 
         $db = $this->dbconnect(); 
         $q = $db->query
-        ('SELECT posts.id, posts.title, posts.author_id, posts.content, users.user_name,
+        ('SELECT id, title, author_id, content, 
         DATE_FORMAT(post_date, \'%d/%m/%Y à %Hh%i\') AS post_date_fr
         FROM posts
-        INNER JOIN users ON posts.author_id = users.id 
         ORDER BY post_date DESC');
         $posts = $q->fetchAll();
         
-        return $posts;
+        return $posts; 
     }
 
     public function getPost($postId) {
 
         $db = $this->dbConnect();
         $q = $db->prepare
-        ('SELECT posts.id, posts.title, posts.author_id, users.user_name, posts.content,
-        DATE_FORMAT(post_date, \'%d/%m/%Y à %Hh%i\') AS post_date_fr
-        FROM posts 
-        INNER JOIN users ON posts.author_id = users.id WHERE posts.id = ?');
+        ('SELECT id, title, content, DATE_FORMAT(post_date, \'%d/%m/%Y à %Hh%i\') AS post_date_fr FROM posts WHERE id = ?');
         $q->execute(array($postId));
         $post = $q->fetch(); 
-        return $post;
-    }
-
-    public function getListPostsAdmin() {
-
-        $db = $this->dbconnect(); 
-        $q = $db->query
-        ('SELECT posts.id, posts.title, posts.author_id, posts.content, users.user_name,
-        DATE_FORMAT(post_date, \'%d/%m/%Y à %Hh%i\') AS post_date_fr,
-        COUNT(comments.id) AS nb_comments
-        FROM posts
-        INNER JOIN users ON posts.author_id = users.id 
-        LEFT JOIN comments
-        ON posts.id = comments.post_id
-        GROUP BY posts.id
-        ORDER BY post_date DESC');
-        $posts = $q->fetchAll();
-        
-        return $posts;
+        return $post; 
     }
 
     public function deletePostAdmin($postId) {
