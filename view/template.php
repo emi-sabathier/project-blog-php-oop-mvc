d652f40ac8ece43b8e3ed3f97a02e900c850896e
emi-sabathier <sabathier.emilie@gmail.com>
2019-04-07 19:08:52 +0200

    AJout favicon

--- view/template.php
+++ view/template.php
@@ -18,6 +18,7 @@
         Un billet simple pour l'Alaska -
         <?= $title ?>
     </title>
+    <link rel="icon" href="public/img/favicon.ico" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
